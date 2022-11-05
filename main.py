adbc3b8a7239352b7be26a5c1ac91ac51f72391c
Santeri Lehtonen <55755203+saneprkl@users.noreply.github.com>
2022-11-05 21:52:59 +0000

    Added web server with keep alive mechanism

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os
 import telebot
+from keep_alive import keep_alive
 
 API_KEY = os.environ['API_KEY']
+bot = telebot.TeleBot(API_KEY)
 
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+  bot.reply_to(message, "Wassup")
+
+
+keep_alive()
+bot.polling()
